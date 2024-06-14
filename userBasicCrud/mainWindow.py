a363fd174feda80fc7229993b6bccc32f16cd0d5
Maze711 <badlonmazeclarion@gmail.com>
2024-06-14 18:40:56 +0800

    User CRUD GUI External Availibilty

---
+++ userBasicCrud/mainWindow.py
@@ -0,0 +1,11 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from userCRUD import Ui_MainWindow
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
